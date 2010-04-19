123e7b81c6827e89514008af4d5c9f15e770ff81
huwshimi <huwshimi@9b1ad702-0ae9-11df-90cd-0fbaec372790>
2010-04-19 06:00:51 +0000

    Added basic feeds file. Pulled out of Verb. Needs plenty of work.

---
+++ djangovoice/feeds.py
@@ -0,0 +1,10 @@
+from django.contrib.syndication.feeds import Feed
+from djangovoice.models import Feedback
+
+class LatestFeedback(Feed):
+    title = "Feedback"
+    link = "/feedback/"
+    description = "Latest feedback"
+
+    def items(self):
+        return Feedback.objects.filter(private=False).order_by('-created')[:10]
